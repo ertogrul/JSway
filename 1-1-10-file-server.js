e370e4b87bcc8c97eb6a68bbbaffc4e7f017d9c4
Michal Kordyzon <michalkordyzon@gmail.com>
2018-04-08 23:33:51 +0200

    123

--- 1-1-10-file-server.js
+++ 1-1-10-file-server.js
@@ -12,3 +12,14 @@ var server = http.createServer(function(req,res) {
 server.listen(thePort, function() { 
    console.log('server is listening');
 });
+
+//#### official #####
+
+var http = require('http')
+var fs = require('fs')
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+  fs.createReadStream(process.argv[3]).pipe(res)
+})
+server.listen(Number(process.argv[2]))
