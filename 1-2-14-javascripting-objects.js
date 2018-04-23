ec653eb5742b2b70b1fe951ab31dde5cbd52b427
ertogrul <michalkordyzon@gmail.com>
2018-04-23 11:21:06 +0200

    boring basics

---
+++ 1-2-14-javascripting-objects.js
@@ -0,0 +1,27 @@
+ var pizza = {
+ 	toppings: ['cheese', 'sauce', 'pepperoni'],
+ 	crust: 'deep dish',
+ 	serves: 2
+ };
+console.log(pizza);
+
+
+
+
+/*
+     var example = {  
+       pizza: 'yummy'  
+     };  
+       
+     console.log(example['pizza']); 
+*/
+//OR
+
+ 
+/*
+ var example = {  
+       pizza: 'yummy'  
+     };  
+       
+     console.log(example.pizza);
+*/
